575ad09c9845cb5afa63ee3a77277b754d2d2fd3
flaviodeaquino <flavio.deaquinof@gmail.com>
2024-04-23 17:24:24 -0300

    módulo 2 - aula 6 a 13

---
+++ Ruby on Rails 5.x - Udemy/Módulo 02 - Ruby Básico/04_relacionais.rb
@@ -0,0 +1,9 @@
+# operadores relacionais: >, >=, <, <=, ==, !=
+
+if 1 > 2
+    puts '1 é maior que 2'
+else
+    puts '1 é menor que 2'
+end
+
+#operador aritméticos: +, -, *, /, ** (potencia), % (resto da divisão)
